9965fd173c2525e5ba30d55bf85d4fbe8ac9f8e9
SIXIVESS <mettatonbb@gmail.com>
2023-03-28 15:45:25 -0700

    Prueba

---
+++ src/main/java/com/mycompany/proyectoagencia_233505_236348/ProyectoAgencia_233505_236348.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.proyectoagencia_233505_236348;
+
+/**
+ *
+ * @author 
+ */
+public class ProyectoAgencia_233505_236348 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/proyectoagencia_233505_236348/Prueba.java
@@ -0,0 +1,9 @@
+package com.mycompany.proyectoagencia_233505_236348;
+
+/**
+ *
+ * @author 
+ */
+public class Prueba {
+    
+}
